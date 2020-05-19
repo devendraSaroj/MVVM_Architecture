4a01b39981754f3e5749ee23fb49d5cf82b18820
devendraSaroj <devendrasaroj97@gmail.com>
2020-05-19 07:31:32 +0530

    Initial commit

---
+++ app/src/main/java/com/devendra_saroj/mvvmapp/ui/auth/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.devendra_saroj.mvvmapp.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.devendra_saroj.mvvmapp.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
